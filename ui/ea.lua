c553cb5a3e967313053b1a82146c20a2ab47072f
Mycu <tomek@mycek.pl>
2025-09-06 20:53:22 +0200

    Optimize widget processing by introducing group-based execution cycles

--- ui/ea.lua
+++ ui/ea.lua
@@ -2,7 +2,10 @@ local widgets
 
 local mapMenu
 
-local external = {};
+local external = {
+    maxGroup = 3, -- Maximum number of groups.
+    cycleCounter = 0 -- Counter for cycle through groups.
+};
 
 local request = require("djfhe.http.request")
 local method = 'POST'
@@ -44,12 +47,42 @@ end
 ---
 function external.fetchData()
     local payload = {}
+    external.cycleCounter = external.cycleCounter + 1
+
+    -- Determine which group to process (1,2,3 and then repeat)
+    local widgetGroupToProcess = external.cycleCounter % external.maxGroup + 1
+
     for key, widget in pairs(widgets) do
-        local output = require(widget.path) -- this will be cached after first load
-        payload[key] = output.handle()
+        for _, group in ipairs(widget.groups) do
+            -- Process only the widgets that belong to the current group
+            if group == widgetGroupToProcess then
+                local output = require(widget.path) -- this will be cached after first load
+                payload[key] = output.handle()
+                break
+            end
+        end
     end
 
-    return payload
+    return external.removeUnsupportedTypes(payload)
+end
+
+---
+--- Remove unsupported types
+---
+function external.removeUnsupportedTypes(value)
+    local elementType = type(value)
+
+    if elementType == "cname" or elementType == "userdata" or elementType == "cdata" then
+        value = nil
+    end
+
+    if elementType == "table" then
+        for k, v in pairs(value) do
+            value[k] = external.removeUnsupportedTypes(v)
+        end
+    end
+
+    return value
 end
 
 init()
--- ui/widgets.lua
+++ ui/widgets.lua
@@ -1,23 +1,36 @@
+-- Application state is refreshed every second.
+-- Each widget is assigned to one or many of the groups limited by maxGroup value.
+-- Group number means in what call it should be executed.
+-- So eg. widget assigned to groups { 1, 2, 3 } will be executed every second.
+-- But widget assigned to group { 3 } will be executed only every third second.
+
 return {
     playerProfile = {
         path = "widgets.player_profile",
-    },
-    activeMission = {
-        path = "widgets.active_mission",
-    },
-    logbook = {
-        path = "widgets.logbook",
+        groups = { 1, 2, 3 },
     },
     missionOffers = {
         path = "widgets.mission_offers",
+        groups = { 1 },
+    },
+    activeMission = {
+        path = "widgets.active_mission",
+        groups = { 1 },
     },
     currentResearch = {
         path = "widgets.current_research",
-    },
-    transactionLog = {
-        path = "widgets.transaction_log",
+        groups = { 1 },
     },
     factions = {
         path = "widgets.factions",
+        groups = { 1 },
+    },
+    transactionLog = {
+        path = "widgets.transaction_log",
+        groups = { 2 },
+    },
+    logbook = {
+        path = "widgets.logbook",
+        groups = { 3 },
     },
 }
